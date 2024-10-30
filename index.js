a120e7a646d0510c4f3e39b29be24f31cc12aeb0
mainman22 <arundhathiramajyam@gmail.com>
2024-10-30 22:41:47 +0530

    Completed rework of To Do List Application

--- index.js
+++ index.js
@@ -1,39 +1,94 @@
-let count = 0;
+const edittask = 0;
 
-function AddCheckbox()
+function AddTask()
 {
     const input = document.getElementById("input");
+    const div = document.getElementById("todo-app");
 
-    if (input.value != '')
-    {
-        const checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
-        checkbox.setAttribute("onclick", "Check(" + count.toString() + ")");
-        checkbox.setAttribute("id", count.toString());
-    
-        const label = document.createElement("label");
-        label.innerHTML = input.value + "    ";
-        label.setAttribute("for", count.toString());
-        label.setAttribute("id", "#" + count.toString());
+    const taskdiv = document.createElement("div");
+    taskdiv.setAttribute("class", "task");
 
-        const button = document.createElement("button");
-        button.innerHTML = "DELETE";
-        button.setAttribute("onclick", "Delete(" + count.toString() + ")");
-        button.setAttribute("id", "##" + count.toString());
-    
-        const div = document.getElementById("div1");
+    const label = document.createElement("label");
+    label.innerHTML = input.value;
+    label.setAttribute("class", "container");
 
-        div.appendChild(checkbox);
-        div.appendChild(label);
-        div.appendChild(button);
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
 
-        var br = document.createElement("br");
-        br.setAttribute("id", "$" + count.toString());
-        div.appendChild(br);
-        
-        count++;
-        input.value = '';
-    }
+    const span = document.createElement("span");
+    span.setAttribute("class", "checkmark")
+
+    const button = document.createElement("button");
+    button.setAttribute("id", "removebutton")
+    button.innerHTML = "Delete";
+
+    const button2 = document.createElement("button");
+    button2.setAttribute("id", "editbutton");
+    button2.innerHTML = "Edit";
+
+    label.appendChild(checkbox);
+    label.appendChild(span);
+
+    taskdiv.appendChild(label);
+    taskdiv.appendChild(button);
+    taskdiv.appendChild(button2);
+    taskdiv.setAttribute("title", "0");
+
+    div.appendChild(taskdiv);
+
+    input.value = '';
+
+    div.addEventListener("click", function(e)
+        {
+            if (e.target.tagName === "INPUT")
+            {
+                if (e.target.checked == true)
+                {
+                    console.log(e.target.parentElement);
+                    e.target.parentElement.style.textDecoration = "line-through";
+                }
+                else
+                {
+                    e.target.parentElement.style.textDecoration = "none";
+                }
+            }
+            else if (e.target.tagName === "BUTTON")
+            {
+                if (e.target.innerHTML === "Delete")
+                {
+                    e.target.parentElement.remove()
+                }
+
+                else if (e.target.innerHTML === "Edit" && e.target.parentElement.title === "0")
+                {
+                    console.log(e.target);
+                    const rowdiv = document.createElement("div");
+                    rowdiv.setAttribute("class", "row");
+
+                    const editinput = document.createElement("input");
+                    editinput.setAttribute("placeholder", "Edit your task");
+                    editinput.setAttribute("id", "editinput");
+
+                    const editbutton = document.createElement("button");
+                    editbutton.innerHTML = "OK";
+                    editbutton.setAttribute("id", "okbutton");
+
+                    rowdiv.appendChild(editinput);
+                    rowdiv.appendChild(editbutton);
+
+                    e.target.parentElement.appendChild(rowdiv);
+                    e.target.parentElement.title = "1";
+                }
+
+                else if (e.target.innerHTML === "OK")
+                {
+                    e.target.parentElement.parentElement.firstChild.firstChild.data = e.target.parentElement.firstChild.value;
+                    e.target.parentElement.parentElement.title = "0";
+                    e.target.parentElement.remove()
+                }
+            }
+        }
+    )
 }
 
 function Check(count)
